2813626fde1e706fdafe3f4869523e447e6ba891
Andray <light.and.ray@gmail.com>
2023-12-07 19:45:49 +0400

    add StableSR's methods

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This simple extention adds native color correction feature from stable-diffusion-webui into "Extras" tab
 
 ![](images/img1.jpg)
+
+If you have installed [StableSR](https://github.com/pkuliyi2015/sd-webui-stablesr) extention, you can choose its color correction methods from dropbox "Method"
--- scripts/color_correction_extras.py
+++ scripts/color_correction_extras.py
@@ -4,6 +4,11 @@ from modules import scripts_postprocessing
 
 NAME = 'Color Correction'
 
+
+def a1111_color_correction(targetImage, sampleImage):
+    return apply_color_correction(setup_color_correction(sampleImage), targetImage)
+
+
 class ColorCorrectionExtras(scripts_postprocessing.ScriptPostprocessing):
     name = NAME
     order = 19000
@@ -12,21 +17,39 @@ class ColorCorrectionExtras(scripts_postprocessing.ScriptPostprocessing):
         with gr.Accordion(NAME, open=False):
             enable = gr.Checkbox(False, label="Enable")
             img = gr.Image(label="Sample Image", source="upload", interactive=True, type="pil", elem_id="image")   
-            useSourceInstead = gr.Checkbox(False, label="Use Source image instead of sample")
+            try:
+                from srmodule.colorfix import adain_color_fix, wavelet_color_fix
+                color_fix = gr.Dropdown(['A1111', 'Wavelet', 'AdaIN'], label="Method", value='A1111')
+            except ImportError:
+                color_fix = gr.Textbox(value='A1111', visible=False)
         args = {
             'img': img,
             'enable': enable,
-            'useSourceInstead' : useSourceInstead,
+            'color_fix' : color_fix,
         }
         return args
 
     def process(self, pp: scripts_postprocessing.PostprocessedImage, **args):
         if args['enable']:
             targetImage = pp.image
-            if args['useSourceInstead']:
-                sampleImage = targetImage
-            else:
-                sampleImage = args['img']
+            sampleImage = args['img']
+            color_fix = args['color_fix']
 
-            pp.image = apply_color_correction(setup_color_correction(sampleImage), targetImage)
-            pp.info[NAME] = True
+            if color_fix == 'A1111':
+                color_correction_func = a1111_color_correction
+            else:
+                from srmodule.colorfix import adain_color_fix, wavelet_color_fix
+                if targetImage.mode == "RGBA":
+                    targetImage = targetImage.convert(mode = "RGB")
+                if sampleImage.mode == "RGBA":
+                    sampleImage = sampleImage.convert(mode = "RGB")
+                if targetImage.size != sampleImage.size:
+                    sampleImage = sampleImage.resize(targetImage.size)
+
+                if color_fix == 'Wavelet':
+                    color_correction_func = wavelet_color_fix
+                else:
+                    color_correction_func = adain_color_fix
+                    
+            pp.image = color_correction_func(targetImage, sampleImage)
+            pp.info[NAME] = color_fix
