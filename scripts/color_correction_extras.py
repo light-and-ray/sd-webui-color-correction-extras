0aad2435792e0b52094616bdbeeb93f73b32cb6c
Andray <light.and.ray@gmail.com>
2023-12-19 22:05:45 +0400

    #1 - extra use all methods

--- scripts/color_correction_extras.py
+++ scripts/color_correction_extras.py
@@ -1,12 +1,43 @@
 from modules.processing import apply_color_correction, setup_color_correction
 import gradio as gr
 from modules import scripts_postprocessing
+import launch
+import packaging.version
+import copy
 
 NAME = 'Color Correction'
+METHODS = ['A1111', 'Wavelet', 'AdaIN']
+
+def extraImagesAvaliable():
+    if packaging.version.parse(launch.git_tag()) >= packaging.version.parse("1.7.0"):
+        return True
+    else:
+        return False
+
 
 
 def a1111_color_correction(targetImage, sampleImage):
     return apply_color_correction(setup_color_correction(sampleImage), targetImage)
+
+
+def applyColorCorrectionMethod(method, targetImage, sampleImage):
+    if method == 'A1111':
+        color_correction_func = a1111_color_correction
+    else:
+        from srmodule.colorfix import adain_color_fix, wavelet_color_fix
+        if targetImage.mode == "RGBA":
+            targetImage = targetImage.convert(mode = "RGB")
+        if sampleImage.mode == "RGBA":
+            sampleImage = sampleImage.convert(mode = "RGB")
+        if targetImage.size != sampleImage.size:
+            sampleImage = sampleImage.resize(targetImage.size)
+
+        if method == 'Wavelet':
+            color_correction_func = wavelet_color_fix
+        else:
+            color_correction_func = adain_color_fix
+        
+    return color_correction_func(targetImage, sampleImage)
 
 
 class ColorCorrectionExtras(scripts_postprocessing.ScriptPostprocessing):
@@ -14,42 +45,40 @@ class ColorCorrectionExtras(scripts_postprocessing.ScriptPostprocessing):
     order = 19000
 
     def ui(self):
+        global METHODS
         with gr.Accordion(NAME, open=False):
             enable = gr.Checkbox(False, label="Enable")
             img = gr.Image(label="Sample Image", source="upload", interactive=True, type="pil", elem_id="image")   
-            try:
-                from srmodule.colorfix import adain_color_fix, wavelet_color_fix
-                color_fix = gr.Dropdown(['A1111', 'Wavelet', 'AdaIN'], label="Method", value='A1111')
-            except ImportError:
-                color_fix = gr.Textbox(value='A1111', visible=False)
+            with gr.Row():
+                try:
+                    from srmodule.colorfix import adain_color_fix, wavelet_color_fix
+                    method = gr.Dropdown(METHODS, label="Method", value='A1111')
+                except ImportError:
+                    METHODS = ['A1111']
+                    method = gr.Textbox(value='A1111', visible=False)
+                extraProcess = gr.Checkbox(False, label="Extra use all methods", visible=extraImagesAvaliable())
         args = {
             'img': img,
             'enable': enable,
-            'color_fix' : color_fix,
+            'method' : method,
+            'extraProcess' : extraProcess,
         }
         return args
 
     def process(self, pp: scripts_postprocessing.PostprocessedImage, **args):
-        if args['enable']:
-            targetImage = pp.image
-            sampleImage = args['img']
-            color_fix = args['color_fix']
+        if args['enable'] == False:
+            return
+        targetImage = copy.copy(pp.image)
+        sampleImage = args['img']
+        method = args['method']
+        extraProcess = args['extraProcess']
+                
+        pp.image = applyColorCorrectionMethod(method, targetImage, sampleImage)
+        pp.info[NAME] = method
 
-            if color_fix == 'A1111':
-                color_correction_func = a1111_color_correction
-            else:
-                from srmodule.colorfix import adain_color_fix, wavelet_color_fix
-                if targetImage.mode == "RGBA":
-                    targetImage = targetImage.convert(mode = "RGB")
-                if sampleImage.mode == "RGBA":
-                    sampleImage = sampleImage.convert(mode = "RGB")
-                if targetImage.size != sampleImage.size:
-                    sampleImage = sampleImage.resize(targetImage.size)
-
-                if color_fix == 'Wavelet':
-                    color_correction_func = wavelet_color_fix
-                else:
-                    color_correction_func = adain_color_fix
-                    
-            pp.image = color_correction_func(targetImage, sampleImage)
-            pp.info[NAME] = color_fix
+        if extraProcess and len(METHODS) > 1 and extraImagesAvaliable():
+            for extraMethod in METHODS:
+                if extraMethod == method:
+                    continue
+                image = applyColorCorrectionMethod(extraMethod, targetImage, sampleImage)
+                pp.extra_images.append(image)
